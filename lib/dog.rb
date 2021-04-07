db95c877540a2b649b075513e043f7a325e50c6b
Shami Tomita <shamitomita@gmail.com>
2021-04-07 17:23:43 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -45,12 +45,13 @@ class Dog
   def update
     sql = "UPDATE dogs SET name = ?, breed = ? WHERE id = ?"
     DB[:conn].execute(sql, self.name, self.breed, self.id)
+    self
   end 
   
   def save
     if self.id 
       self.update
-      return self
+      
     else 
       sql =<<-SQL
       INSERT INTO dogs  (name, breed)
