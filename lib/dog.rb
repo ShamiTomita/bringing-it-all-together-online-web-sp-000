41457649e060df69c81545265b38788abe8ab47d
Shami Tomita <shamitomita@gmail.com>
2021-04-07 17:22:20 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -59,7 +59,7 @@ class Dog
       DB[:conn].execute(sql, self.name, self.breed)
       @id = DB[:conn].execute("SELECT last_insert_rowid() FROM dogs")[0][0]
     end 
-    binding.pry
+  
   end 
   
   def self.find_by_name(name) 
