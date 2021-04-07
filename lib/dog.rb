37d95e9502a876ac694e035d923afcc68700d19f
Shami Tomita <shamitomita@gmail.com>
2021-04-07 17:16:10 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -27,8 +27,8 @@ class Dog
     DB[:conn].execute(sql)
   end 
   
-  def self.create(id, name:, breed:)
-    dog = Dog.new(id, name, breed)
+  def self.create(name, breed)
+    dog = Dog.new(name, breed)
     dog.save
     dog
   end 
